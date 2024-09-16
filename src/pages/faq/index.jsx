86b171c9ab7797928045329589d2a008152e9b8c
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-09-16 09:27:44 +0300

    Calendar And FAQ

--- src/pages/faq/index.jsx
+++ src/pages/faq/index.jsx
@@ -1,8 +1,88 @@
 import React from 'react'
+import { token } from "../../theme/theme";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from '@mui/material'
+import { ExpandMoreOutlined } from '@mui/icons-material';
+import { useTheme } from '@emotion/react';
+import Header from '../../components/global/Header';
 
 const FAQ = () => {
+
+    const theme = useTheme();
+    const colors = token(theme.palette.mode);
+
+
     return (
-        <div>FAQ</div>
+        <Box m='20px'>
+            <Header title="FAQ" subtitle={'Frequently Asked Questions Page'} />
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreOutlined />}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        An Important Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreOutlined />}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        Another Important Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreOutlined />}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        Your Favorite Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreOutlined />}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        Some Random Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreOutlined />}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        The Final Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+        </Box>
     )
 }
 
